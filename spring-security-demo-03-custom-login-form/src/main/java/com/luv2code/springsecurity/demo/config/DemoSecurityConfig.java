6bbf55acf3b820336b2274b53ec43c40612e0c92
Denis Kisina <kisinad@gmail.com>
2020-08-01 15:02:19 -0500

    configured customized login form

--- spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,7 +2,6 @@ package com.luv2code.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -20,13 +19,6 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.inMemoryAuthentication().withUser(user.username("john").password("test123").roles("EMPLOYEE"))
 				.withUser(user.username("mary").password("test123").roles("MANAGER"))
 				.withUser(user.username("suzan").password("test123").roles("ADMIN"));
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
-		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("showLoginPage")
-				.loginProcessingUrl("/authenticateTheUser").permitAll();
 	}
 
 }
